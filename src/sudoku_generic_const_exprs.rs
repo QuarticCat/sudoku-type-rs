d96a2ce27ebb460bb761dea33cb467d5d1b829fd
QuarticCat <QuarticCat@pm.me>
2025-05-27 10:48:41 +0800

    Simplify sudoku_generic_const_exprs.rs

--- src/sudoku_generic_const_exprs.rs
+++ src/sudoku_generic_const_exprs.rs
@@ -1,105 +1,57 @@
-#![feature(generic_const_exprs)]
-#![feature(adt_const_params)]
+#![allow(dead_code)]
 
-type CellType = u8;
-
-struct Assert<const COND: bool>;
-
-trait IsTrue {}
-impl IsTrue for Assert<true> {}
-
-/*
-#[derive(Debug, Clone, Copy, PartialEq, Eq, ConstParamTy)]
+#[derive(Debug, Clone, Copy)]
 enum Num {
-    One = 1,
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
+    _1 = (1 << 0),
+    _2 = (1 << 1),
+    _3 = (1 << 2),
+    _4 = (1 << 3),
+    _5 = (1 << 4),
+    _6 = (1 << 5),
+    _7 = (1 << 6),
+    _8 = (1 << 7),
+    _9 = (1 << 8),
+    __ = (1 << 9),
+}
+use Num::*;
+
+macro_rules! const_for {
+    ($i:ident in $range:expr => $body:stmt) => {{
+        let mut $i = $range.start;
+        while $i < $range.end {
+            $body
+            $i += 1;
+        }
+    }};
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, ConstParamTy)]
-enum SudokuCell {
-    Filled(Num),
-    Empty,
+const fn check_sudoku(array: [[Num; 9]; 9]) {
+    const_for!(i in 0..9 => {
+        let (mut 横行, mut 縦列, mut ブロック) = (0, 0, 0);
+        const_for!(j in 0..9 => {
+            横行 |= array[i][j] as i32;
+            縦列 |= array[j][i] as i32;
+            ブロック |= array[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3] as i32;
+        });
+        if 横行 != 0b111111111 || 縦列 != 0b111111111 || ブロック != 0b111111111 {
+            panic!("invalid");
+        }
+    });
 }
-
-const fn not_eq(a: SudokuCell, b: SudokuCell) -> bool {
-    match (a, b) {
-        (SudokuCell::Filled(x), SudokuCell::Filled(y)) => x as u8 != y as u8,
-        (SudokuCell::Empty, SudokuCell::Empty) => true,
-        (SudokuCell::Empty, SudokuCell::Filled(_)) | (SudokuCell::Filled(_), SudokuCell::Empty) => true,
-    }
-}
-*/
-
-pub struct Sudoku<const ARRAY: [[CellType; 9]; 9]>
-where Assert<{ sudoku_is_valid(ARRAY) }>: IsTrue;
-
-const fn are_diff_type_params(x1: CellType, x2: CellType, x3: CellType, x4: CellType, x5: CellType, x6: CellType, x7: CellType, x8: CellType, x9: CellType) -> bool { // Enumなどを使う場合は、その型用にmatchなどで不等を示すconst関数を実装する
-    x1 != x2 && x1 != x3 && x1 != x4 && x1 != x5 && x1 != x6 && x1 != x7 && x1 != x8 && x1 != x9
-    && x2 != x3 && x2 != x4 && x2 != x5 && x2 != x6 && x2 != x7 && x2 != x8 && x2 != x9
-    && x3 != x4 && x3 != x5 && x3 != x6 && x3 != x7 && x3 != x8 && x3 != x9
-    && x4 != x5 && x4 != x6 && x4 != x7 && x4 != x8 && x4 != x9
-    && x5 != x6 && x5 != x7 && x5 != x8 && x5 != x9
-    && x6 != x7 && x6 != x8 && x6 != x9
-    && x7 != x8 && x7 != x9
-    && x8 != x9
-}
-
-const fn sudoku_is_valid(array: [[CellType; 9]; 9]) -> bool {
-    ( // 横行
-        are_diff_type_params(array[0][0], array[0][1], array[0][2], array[0][3], array[0][4], array[0][5], array[0][6], array[0][7], array[0][8])
-        && are_diff_type_params(array[1][0], array[1][1], array[1][2], array[1][3], array[1][4], array[1][5], array[1][6], array[1][7], array[1][8])
-        && are_diff_type_params(array[2][0], array[2][1], array[2][2], array[2][3], array[2][4], array[2][5], array[2][6], array[2][7], array[2][8])
-        && are_diff_type_params(array[3][0], array[3][1], array[3][2], array[3][3], array[3][4], array[3][5], array[3][6], array[3][7], array[3][8])
-        && are_diff_type_params(array[4][0], array[4][1], array[4][2], array[4][3], array[4][4], array[4][5], array[4][6], array[4][7], array[4][8])
-        && are_diff_type_params(array[5][0], array[5][1], array[5][2], array[5][3], array[5][4], array[5][5], array[5][6], array[5][7], array[5][8])
-        && are_diff_type_params(array[6][0], array[6][1], array[6][2], array[6][3], array[6][4], array[6][5], array[6][6], array[6][7], array[6][8])
-        && are_diff_type_params(array[7][0], array[7][1], array[7][2], array[7][3], array[7][4], array[7][5], array[7][6], array[7][7], array[7][8])
-        && are_diff_type_params(array[8][0], array[8][1], array[8][2], array[8][3], array[8][4], array[8][5], array[8][6], array[8][7], array[8][8])
-    ) &&
-    ( // 縦列
-        are_diff_type_params(array[0][0], array[1][0], array[2][0], array[3][0], array[4][0], array[5][0], array[6][0], array[7][0], array[8][0])
-        && are_diff_type_params(array[0][1], array[1][1], array[2][1], array[3][1], array[4][1], array[5][1], array[6][1], array[7][1], array[8][1])
-        && are_diff_type_params(array[0][2], array[1][2], array[2][2], array[3][2], array[4][2], array[5][2], array[6][2], array[7][2], array[8][2])
-        && are_diff_type_params(array[0][3], array[1][3], array[2][3], array[3][3], array[4][3], array[5][3], array[6][3], array[7][3], array[8][3])
-        && are_diff_type_params(array[0][4], array[1][4], array[2][4], array[3][4], array[4][4], array[5][4], array[6][4], array[7][4], array[8][4])
-        && are_diff_type_params(array[0][5], array[1][5], array[2][5], array[3][5], array[4][5], array[5][5], array[6][5], array[7][5], array[8][5])
-        && are_diff_type_params(array[0][6], array[1][6], array[2][6], array[3][6], array[4][6], array[5][6], array[6][6], array[7][6], array[8][6])
-        && are_diff_type_params(array[0][7], array[1][7], array[2][7], array[3][7], array[4][7], array[5][7], array[6][7], array[7][7], array[8][7])
-        && are_diff_type_params(array[0][8], array[1][8], array[2][8], array[3][8], array[4][8], array[5][8], array[6][8], array[7][8], array[8][8])
-    ) &&
-    ( // ブロック
-        are_diff_type_params(array[0][0], array[0][1], array[0][2], array[1][0], array[1][1], array[1][2], array[2][0], array[2][1], array[2][2])
-        && are_diff_type_params(array[0][3], array[0][4], array[0][5], array[1][3], array[1][4], array[1][5], array[2][3], array[2][4], array[2][5])
-        && are_diff_type_params(array[0][6], array[0][7], array[0][8], array[1][6], array[1][7], array[1][8], array[2][6], array[2][7], array[2][8])
-        && are_diff_type_params(array[3][0], array[3][1], array[3][2], array[4][0], array[4][1], array[4][2], array[5][0], array[5][1], array[5][2])
-        && are_diff_type_params(array[3][3], array[3][4], array[3][5], array[4][3], array[4][4], array[4][5], array[5][3], array[5][4], array[5][5])
-        && are_diff_type_params(array[3][6], array[3][7], array[3][8], array[4][6], array[4][7], array[4][8], array[5][6], array[5][7], array[5][8])
-        && are_diff_type_params(array[6][0], array[6][1], array[6][2], array[7][0], array[7][1], array[7][2], array[8][0], array[8][1], array[8][2])
-        && are_diff_type_params(array[6][3], array[6][4], array[6][5], array[7][3], array[7][4], array[7][5], array[8][3], array[8][4], array[8][5])
-        && are_diff_type_params(array[6][6], array[6][7], array[6][8], array[7][6], array[7][7], array[7][8], array[8][6], array[8][7], array[8][8])
-    )
-}
-
 
 fn main() {
-    let sudoku = Sudoku::<{[
-        [5, 3, 4,  6, 7, 8,  9, 1, 2],
-        [6, 7, 2,  1, 9, 5,  3, 4, 8],
-        [1, 9, 8,  3, 4, 2,  5, 6, 7],
+    #[rustfmt::skip]
+    const _: () = check_sudoku([
+        [_5, _3, _4,  _6, _7, _8,  _9, _1, _2],
+        [_6, _7, _2,  _1, _9, _5,  _3, _4, _8],
+        [_1, _9, _8,  _3, _4, _2,  _5, _6, _7],
 
-        [8, 5, 9,  7, 6, 1,  4, 2, 3],
-        [4, 2, 6,  8, 5, 3,  7, 9, 1],
-        [7, 1, 3,  9, 2, 4,  8, 5, 6],
+        [_8, _5, _9,  _7, _6, _1,  _4, _2, _3],
+        [_4, _2, _6,  _8, _5, _3,  _7, _9, _1],
+        [_7, _1, _3,  _9, _2, _4,  _8, _5, _6],
 
-        [9, 6, 1,  5, 3, 7,  2, 8, 4],
-        [2, 8, 7,  4, 1, 9,  6, 3, 5],
-        [3, 4, 5,  2, 8, 6,  1, 7, 9],
-    ]}>;
+        [_9, _6, _1,  _5, _3, _7,  _2, _8, _4],
+        [_2, _8, _7,  _4, _1, _9,  _6, _3, _5],
+        [_3, _4, _5,  _2, _8, _6,  _1, _7, _9],
+    ]);
 }
